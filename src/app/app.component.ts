bbf9516e50bf5287332472d9cfa081f8124b4094
Swapnil Opash <sra.opash@gmail.com>
2023-12-19 11:35:42 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { SharedService } from './@shared/services/shared.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Meta } from '@angular/platform-browser';
+import { SocketService } from './@shared/services/socket.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'freedom-ssr';
+  showButton = false;
+  tab: any;
+
+  constructor(
+    private sharedService: SharedService,
+    private spinner: NgxSpinnerService,
+    private socketService: SocketService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+
+    this.checkDocumentFocus()
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.sharedService.getUserDetails();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.spinner.hide();
+    setTimeout(() => {
+      const splashScreenLoader = document.getElementById('splashScreenLoader');
+      if (splashScreenLoader) {
+        splashScreenLoader.style.display = 'none';
+      }
+    }, 1000);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY > 300) {
+      this.showButton = true;
+    } else {
+      this.showButton = false;
+    }
+  }
+
+  scrollToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  @HostListener('document:visibilitychange', ['$event']) checkDocumentFocus() {
+    if (!window.document.hidden) {
+      if (this.tab) {
+        clearInterval(this.tab);  
+      }
+      if (!this.socketService.socket?.connected) {
+        this.socketService.socket?.connect();
+        const profileId = +localStorage.getItem('profileId');
+        this.socketService.socket?.emit('join', { room: profileId });
+      }
+    } else {
+      this.tab = setInterval(() => {
+        if (!this.socketService.socket?.connected) {
+          this.socketService.socket?.connect();
+          const profileId = +localStorage.getItem('profileId');
+          this.socketService.socket?.emit('join', { room: profileId });
+        }
+      }, 3000)
+
+    }
+  }
+}
---
+++ src/app/layouts/main-layout/pages/notifications/notification.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
+import { ToastService } from 'src/app/@shared/services/toast.service';
+import { CustomerService } from 'src/app/@shared/services/customer.service';
+import { SeoService } from 'src/app/@shared/services/seo.service';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss'],
+})
+export class NotificationsComponent {
+  notificationList: any[] = [];
+
+  constructor(
+    private customerService: CustomerService,
+    private spinner: NgxSpinnerService,
+    private router: Router,
+    private toastService: ToastService,
+    private seoService: SeoService
+
+  ) 
+  {  const data = {
+    title: 'Freedom.Buzz Notification',
+    url: `${location.href}`,
+    description: '',
+  };
+  this.seoService.updateSeoMetaData(data);}
+
+  ngOnInit(): void {
+    this.getNotificationList();
+  }
+
+  getNotificationList() {
+    this.spinner.show();
+    const id = localStorage.getItem('profileId');
+    this.customerService.getNotificationList(Number(id)).subscribe(
+      {
+        next: (res: any) => {
+          this.spinner.hide();
+          this.notificationList = res?.data;
+        },
+        error:
+          (error) => {
+            this.spinner.hide();
+            console.log(error);
+          }
+      });
+  }
+
+  viewUserPost(id) {
+    this.router.navigate([`post/${id}`]);
+  }
+
+  removeNotification(id: number): void {
+    this.customerService.deleteNotification(id).subscribe({
+      next: (res: any) => {
+        this.toastService.success(res.message || 'Notification delete successfully');
+        this.getNotificationList();
+      },
+    });
+  }
+
+  readUnreadNotification(id, isRead): void {
+    this.customerService.readUnreadNotification(id, isRead).subscribe({
+      next: (res) => {
+        this.toastService.success(res.message);
+        this.getNotificationList();
+      },
+    });
+  }
+}
