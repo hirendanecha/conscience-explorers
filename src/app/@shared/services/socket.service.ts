803cb1ab4b924a6a10b34065d3584f2ad5da4422
Swapnil Opash <sra.opash@gmail.com>
2024-02-29 16:15:52 +0530

    fixed

--- src/app/@shared/services/socket.service.ts
+++ src/app/@shared/services/socket.service.ts
@@ -12,18 +12,24 @@ export class SocketService {
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
       const token = localStorage.getItem('auth-token');
-      this.socket = io(environment.socketUrl, {
-        reconnectionDelay: 100,
-        reconnectionDelayMax: 300,
-        // reconnection: true,
-        randomizationFactor: 0.2,
-        // timeout: 120000,
-        reconnectionAttempts: 50000,
-        transports: ['websocket'],
-      });
+      if (token) {
+        const customHeaders = {
+          Authorization: `Bearer ${token}`,
+        };
+        this.socket = io(environment.socketUrl, {
+          reconnectionDelay: 100,
+          reconnectionDelayMax: 300,
+          // reconnection: true,
+          randomizationFactor: 0.2,
+          // timeout: 120000,
+          reconnectionAttempts: 50000,
+          transports: ['websocket'],
+          auth: customHeaders,
+        });
+      }
     }
   }
-  
+
   connect(): void {
     if (isPlatformBrowser(this.platformId)) {
       const token = localStorage.getItem('auth-token');
